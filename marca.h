fb954cb193b3131842afd337aa8fe11e373f0a81
Belen Martinez <60630360+belenmartineztala@users.noreply.github.com>
2021-10-06 17:53:14 -0300

    Add files via upload

---
+++ marca.h
@@ -0,0 +1,19 @@
+/*
+ * marca.h
+ *
+ *  Created on: 6 oct. 2021
+ *      Author: Belen
+ */
+
+#ifndef MARCA_H_
+#define MARCA_H_
+
+typedef struct
+{
+    int IdMarcas[];
+    char descripcion[20];
+    int isEmpty;
+} eMarcas;
+
+#endif /* MARCA_H_ */
+
